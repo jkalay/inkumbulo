3c7fcbc88ef7784333530084ec2709dcd2252789
Jack Kalayjian <jkalay@gmail.com>
2020-02-13 12:48:45 -0800

    Implement card shuffling functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,21 @@ var firstCardClicked = null;
 var secondCardClicked = null;
 var firstCardClasses;
 var secondCardClasses;
+var animals = [
+  "crocodile",
+  "dog",
+  "elephant",
+  "giraffe",
+  "hippo",
+  "leopard",
+  "lion",
+  "rhino",
+  "zebra"
+];
+var deck = animals.concat(animals);
 
+shuffle();
+addCardClassesToDOM();
 gameBoard.addEventListener("click", handleClick);
 
 function handleClick(event) {
@@ -17,8 +31,7 @@ function handleClick(event) {
   if (firstCardClicked == null) {
     firstCardClicked = event.target;
     firstCardClasses = firstCardClicked.previousElementSibling.className;
-  }
-  else {
+  } else {
     gameBoard.removeEventListener("click", handleClick);
     secondCardClicked = event.target;
     secondCardClasses = secondCardClicked.previousElementSibling.className;
@@ -26,8 +39,7 @@ function handleClick(event) {
     if (firstCardClasses == secondCardClasses) {
       gameBoard.addEventListener("click", handleClick);
       firstCardClicked = secondCardClicked = null;
-    }
-    else {
+    } else {
       setTimeout(function delay() {
         firstCardClicked.parentElement.classList.remove("is-flipped");
         secondCardClicked.parentElement.classList.remove("is-flipped");
@@ -35,6 +47,21 @@ function handleClick(event) {
         gameBoard.addEventListener("click", handleClick);
       }, 1500);
     }
-
   }
+}
+
+function shuffle() {
+  // Fisher-Yates
+  for (let i = deck.length - 1; i > 0; i--) {
+    let rand = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[rand]] = [deck[rand], deck[i]];
+  }
+}
+
+function addCardClassesToDOM() {
+  var cards = document.querySelectorAll(".card-front");
+
+  cards.forEach((card, index) => {
+    card.classList.add(`card-${deck[index]}`);
+  })
 }
