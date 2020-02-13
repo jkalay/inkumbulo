0ce5299161e70e8d6a5162b5ce98bb383b1f9e51
Jack Kalayjian <jkalay@gmail.com>
2020-02-13 03:38:09 -0800

    Add card matching functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,40 @@
-var cards = document.getElementById("cards");
+var gameBoard = document.getElementById("cards");
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardClasses;
+var secondCardClasses;
 
-cards.addEventListener("click", function handleClick(event) {
+gameBoard.addEventListener("click", handleClick);
+
+function handleClick(event) {
   if (!event.target.className.includes("card-back")) {
     return;
   }
 
-  var card = event.target;
-  card.className += "hide";
-});
+  var flipCard = event.target;
+  flipCard.className += " hide";
+
+  if (firstCardClicked == null) {
+    firstCardClicked = event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  }
+  else {
+    gameBoard.removeEventListener("click", handleClick);
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+
+    if (firstCardClasses == secondCardClasses) {
+      gameBoard.addEventListener("click", handleClick);
+      firstCardClicked = secondCardClicked = null;
+    }
+    else {
+      setTimeout(function delay() {
+        firstCardClicked.classList.remove("hide");
+        secondCardClicked.classList.remove("hide");
+        firstCardClicked = secondCardClicked = null;
+        gameBoard.addEventListener("click", handleClick);
+      }, 1500);
+    }
+
+  }
+}
